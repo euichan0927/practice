ae16cb5b2b500978e290993a5e3e66e2bbb1c05b
김의찬 <yuichan99@naver.com>
2023-06-26 13:43:32 +0900

    django practice day2

--- practice/settings.py
+++ practice/settings.py
@@ -12,15 +12,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os, environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+environ.Env.read_env(
+    env_file=(os.path.join(BASE_DIR, '.env'))
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v21(f^wy3a)@h4mw_2#7$^q7sl$9jkiqy97)wly2rlc#24kg@9'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
